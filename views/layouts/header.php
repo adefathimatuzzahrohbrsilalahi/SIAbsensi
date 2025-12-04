78fed0577541f715a2ed6abd11ae951e387ae0e7
n4days <alirsyhad.ramadhan49@gmail.com>
2025-12-04 19:38:41 +0700

    fix: mengubah bootstrap dari cdn ke local

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,19 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Sistem Informasi Absensi MAN2 Asahan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             padding-top: 60px;
-        }
-
-        .brand {
-            font-weight: 700;
-        }
-
-        .card-sm {
-            border-radius: 12px;
-            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.06);
         }
     </style>
 </head>
---
+++ views/layouts/scripts.php
@@ -0,0 +1,2 @@
+<script src="../assets/bootstrap/js/jquery.min.js"></script>
+<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
